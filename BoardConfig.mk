acd64c7cf1e0083c50ea78bd5bcd5407edba7a32
Paul Clark <clarkbrunswick55@gmail.com>
2016-06-26 11:46:04 -0400

    switched toolchains

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,14 +133,14 @@ TARGET_GCC_VERSION_EXP := 4.9
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
-TARGET_GCC_VERSION_ARM64 := 6.x
+TARGET_GCC_VERSION_ARM64 := 5.x
 
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := owl_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-6.x-kernel/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-5.x-kernel/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 
 # Uber Optimizations
