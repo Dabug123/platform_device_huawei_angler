250fe99399dfaec197cce0b65a7eac19fd517aa7
Josh Fox <joshfox87@gmail.com>
2016-07-20 23:26:22 -0400

    fix kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=angler androidboot.selinux=permissive androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=angler androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 BOARD_USES_ALSA_AUDIO := true
@@ -136,7 +136,7 @@ EXTENDED_FONT_FOOTPRINT := true
 TARGET_GCC_VERSION_ARM64 := 4.9
 
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
-TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_CONFIG := owl_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Kernel Toolchain
